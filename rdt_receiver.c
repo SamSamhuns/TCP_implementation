11889b03808e62926ab979a56550dceb629950c4
sms1198 <sms1198@nyu.edu>
2018-04-01 12:22:01 -0700

    TASK I commit project2-Teona-Sam
    Added important comments in code

--- rdt_receiver.c
+++ rdt_receiver.c
@@ -1,4 +1,5 @@
 /*
+ * Starter code modified by Samridha sms1198 and Teona tr1328
  * Nabil Rahiman
  * NYU Abudhabi
  * email: nr83@nyu.edu
@@ -18,13 +19,6 @@
 #include "common.h"
 #include "packet.h"
 
-
-/*
- * You ar required to change the implementation to support
- * window size greater than one.
- * In the currenlt implemenetation window size is one, hence we have
- * onlyt one send and receive packet
- */
 tcp_packet *recvpkt;
 tcp_packet *sndpkt;
 
@@ -102,8 +96,13 @@ int main(int argc, char **argv) {
         }
         recvpkt = (tcp_packet *) buffer;
         assert(get_data_size(recvpkt) <= DATA_SIZE);
-        if ( recvpkt->hdr.data_size == 0) {
-            //VLOG(INFO, "End Of File has been reached");
+
+        // ***********************************************************************************************************************
+        // NOTE TO NABIL
+        // On rare occurences, the receiver does not exit because it never receives a recvpkt with a data_size of 0
+
+        if ( recvpkt->hdr.data_size == 0 ) {
+            VLOG(INFO, "End Of File has been reached");
             fclose(fp);
             break;
         }
@@ -111,37 +110,41 @@ int main(int argc, char **argv) {
         /* 
          * sendto: ACK back to the client 
          */
+        
         gettimeofday(&tp, NULL);
-        VLOG(DEBUG, "%lu, %d, %d", tp.tv_sec, recvpkt->hdr.data_size, recvpkt->hdr.seqno);
+        VLOG(DEBUG, "%lu, %d, %d", tp.tv_sec, recvpkt->hdr.data_size, recvpkt->hdr.seqno, recvpkt->hdr.ackno);
 
         // Checking if packets are received in order, if not, send ACK for the original package
+        printf("next seqno = %i and recvpkt->hdr.seqno = %i\n", next_seqno, recvpkt->hdr.seqno );
         if ( next_seqno != recvpkt->hdr.seqno )
         {
             sndpkt = make_packet(0);
+            // Ask for the same old packet
             sndpkt->hdr.ackno = next_seqno;
             sndpkt->hdr.ctr_flags = ACK;
             if (sendto(sockfd, sndpkt, TCP_HDR_SIZE, 0, 
                     (struct sockaddr *) &clientaddr, clientlen) < 0) {
                 error("ERROR in sendto");
             }
+            VLOG(INFO, "Dropping out of order Packet\n\n");
+            printf("DROP PACKAGE LOOP next seqno = %i and recvpkt->hdr.seqno = %i and sndpck hdr ACK NO %i \n\n", next_seqno, recvpkt->hdr.seqno,sndpkt->hdr.ackno );
         }
+        // If the packet is received in order
         else
         {
             fseek(fp, recvpkt->hdr.seqno, SEEK_SET);
             fwrite(recvpkt->data, 1, recvpkt->hdr.data_size, fp);
             sndpkt = make_packet(0);
             sndpkt->hdr.ackno = recvpkt->hdr.seqno + recvpkt->hdr.data_size;
-            next_seqno = sndpkt->hdr.ackno;
+            next_seqno = sndpkt->hdr.ackno; // Update next_seqno to the hdr.ackno
+
+            printf("SEND PACKAGE LOOP next seqno = %i and recvpkt->hdr.seqno = %i and sndpck hdr ACK NO %i \n\n", next_seqno, recvpkt->hdr.seqno,sndpkt->hdr.ackno );
             sndpkt->hdr.ctr_flags = ACK;
             if (sendto(sockfd, sndpkt, TCP_HDR_SIZE, 0, 
                     (struct sockaddr *) &clientaddr, clientlen) < 0) {
                 error("ERROR in sendto");
             }
-        }
-
-
-
+        }  
     }
-
     return 0;
 }
