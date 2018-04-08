8503db09221bcf048f90c69662059a3fc12fcdb9
sms1198 <sms1198@nyu.edu>
2018-04-08 08:24:02 -0700

    Added Pop and Push functions for linked list
    Added the Pop function to remove the first node packet in the linked list.
    Added the Push function to add a new node packet to the end of the linked list.

--- packet.c
+++ packet.c
@@ -1,4 +1,5 @@
 /*
+ * Modified by Samridha sms1198 and Teona tr1348
  * Nabil Rahiman
  * NYU Abudhabi
  * email: nr83@nyu.edu
@@ -25,3 +26,34 @@ int get_data_size(tcp_packet *pkt)
     return pkt->hdr.data_size;
 }
 
+
+// Adds a tcp_packet buffer to the end of the list
+void push ( nodepkt * head, tcp_packet *buffer_pkt ) 
+{
+	nodepkt * current = head;
+	while ( current->next_nodepkt != NULL ) {
+		current = current->next_nodepkt;
+	}
+
+	current->next_nodepkt = malloc(sizeof(nodepkt));
+	current->next_nodepkt->buffer_pkt = (tcp_packet*) buffer_pkt;
+	current->next_nodepkt->next_nodepkt = NULL;
+}; 
+
+// Removes the first item from the list
+// And returns the packet that was initially stored at the head of the linked list 
+int pop (nodepkt ** head) {
+    int ret_val = -1;
+    nodepkt *following_nodepkt = NULL;
+
+    if (*head == NULL) {
+        return -1;
+    }
+
+    following_nodepkt= (*head)->next_nodepkt;
+    ret_val = (*head)-> buffer_pkt;
+    free(*head);
+    *head = following_nodepkt;
+
+    return ret_val;
+}
