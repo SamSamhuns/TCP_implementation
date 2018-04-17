d324f2ea1de5495f7bfec019616d7661c507d011
sms1198 <sms1198@nyu.edu>
2018-04-17 04:17:05 -0700

    Removed extra make_packet() calls in receiver
    Removed the unnnecessary make_packet calls in receiver for a smoothed out flow for sending acks for each packet received from the sender.

--- rdt_receiver.c
+++ rdt_receiver.c
@@ -40,7 +40,7 @@ int main(int argc, char **argv) {
     int iter = 0; // Iterator variable
     while ( iter < buffer_size ) {
         receiver_buffer[iter] = make_packet(MSS_SIZE);
-        receiver_buffer[iter]->hdr.data_size = 0; // Assign all created packets to have a data_size of 0
+        receiver_buffer[iter]->hdr.data_size = 0; // Assign all created packets to have a data_size of 0 for empty packets
         receiver_buffer[iter]->hdr.seqno = -1; // Assign all packet hdr.seqno's to -1 for exmpty packets
         iter++;
     }
@@ -94,7 +94,6 @@ int main(int argc, char **argv) {
      * main loop: wait for a datagram, then echo it
      */
     VLOG(DEBUG, "epoch time, bytes received, sequence number");
-
     clientlen = sizeof(clientaddr);
     while (1) {
         /*
@@ -111,56 +110,31 @@ int main(int argc, char **argv) {
         // ***********************************************************************************************************************
         // NOTE TO NABIL
         // On rare occurences, the receiver does not exit because it never receives a recvpkt with a data_size of 0
-
         if ( recvpkt->hdr.data_size == 0 ) {
             VLOG(INFO, "End Of File has been reached");
             fclose(fp);
             break;
         }
-
-        /* TEST ORINT CODE FOR DEBUG PURPOSES
-        int ipt =0;
-        while ( ipt < buffer_size) {
-            printf("The hdr.seqno values in the array are %i\n", receiver_buffer[ipt]->hdr.seqno );
-            ipt++;
-
-        }
-        */
-
-        /* 
-         * sendto: ACK back to the client 
-         */
+        
         gettimeofday(&tp, NULL);
         VLOG(DEBUG, "%lu, %d, %d", tp.tv_sec, recvpkt->hdr.data_size, recvpkt->hdr.seqno);
 
-        // Checking if packets are received in order, if not, send ACK for the original package
-        //printf("next seqno = %i and recvpkt->hdr.seqno = %i\n", next_seqno, recvpkt->hdr.seqno );
-        if ( next_seqno < recvpkt->hdr.seqno )
-        {
-            sndpkt = make_packet(0);
-            // Ask for the same old packet
-            sndpkt->hdr.ackno = next_seqno;
-            sndpkt->hdr.ctr_flags = ACK;
-            if (sendto(sockfd, sndpkt, TCP_HDR_SIZE, 0, 
-                    (struct sockaddr *) &clientaddr, clientlen) < 0) {
-                error("ERROR in sendto");
-            }
-
-            printf("Next expected seqno in receiver is %i \n", next_seqno );
-            printf("Current packet received from sender has seqno %i \n", recvpkt->hdr.seqno );
+        // If a packet with a higher seqno than currently expected is received 
+        // check if the packet can be stored in the receiver buffer
+        if ( next_seqno < recvpkt->hdr.seqno ) {
+            //printf("Next expected seqno in receiver is %i \n", next_seqno );
             /*
-            *
             * For the receiver buffer, empty buffer packet seqno is -1 with data_size 0 so  
             * the rcvpkt's seqno number does not match with empty packet seqno's
             */
+
             // Check for duplicate buffered packets in the receiver buffer
             iter = 0;
             int duplicate = 0;
             int buffer_full = 1;
             while ( iter<buffer_size ) {
-                if ( receiver_buffer[iter]->hdr.seqno == recvpkt->hdr.seqno )
-                {
-                    printf("Duplicate packet not added to present buffer\n");
+                if ( receiver_buffer[iter]->hdr.seqno == recvpkt->hdr.seqno ) {
+                    //printf("Duplicate packet not added to present buffer\n");
                     duplicate = 1;
                     break;
                 }
@@ -168,8 +142,7 @@ int main(int argc, char **argv) {
             }
             
             // Only continue processing rcvpkt if it is not a duplicate otherwise drop it
-            if ( duplicate == 0 ) 
-            {
+            if ( duplicate == 0 ) {
                 // Check if buffer is filled up
                 iter = 0;
                 while ( iter<buffer_size ) {
@@ -177,7 +150,7 @@ int main(int argc, char **argv) {
                     if ( receiver_buffer[iter]->hdr.seqno == -1 && receiver_buffer[iter]->hdr.data_size == 0 )
                     {
                         memcpy(receiver_buffer[iter], recvpkt, recvpkt->hdr.data_size); 
-                        printf("Recvpkt seqno %i added to the receiver_buffer\n", recvpkt->hdr.seqno); 
+                        //printf("Recvpkt seqno %i added to the receiver_buffer\n", recvpkt->hdr.seqno); 
                         buffer_full = 0;           
                         break;
                     }
@@ -185,70 +158,59 @@ int main(int argc, char **argv) {
                 }
             }
             else {
-                VLOG(INFO, "Out of order Packet Dropped\n\n");
+                //VLOG(INFO, "Out of order Packet Dropped\n\n");
             }
-            if ( buffer_full == 1 && duplicate == 0 ) // Buffer full and no duplicates
-            {
-                VLOG(INFO, "Buffer is full: Packet Dropped\n\n");
+            if ( buffer_full == 1 && duplicate == 0 ) { // Buffer full and no duplicates
+                //VLOG(INFO, "Buffer is full: Packet Dropped\n\n");
             }
-            //printf("Next seqno = %i and recvpkt->hdr.seqno = %i and sndpck hdr ACK NO %i \n\n", next_seqno, recvpkt->hdr.seqno,sndpkt->hdr.ackno ); 
-         }
+        }
 
         // If the packet is received in order
-        if (next_seqno == recvpkt->hdr.seqno )
-        {
+        else if ( next_seqno == recvpkt->hdr.seqno ) {
             fseek(fp, recvpkt->hdr.seqno, SEEK_SET);
             fwrite(recvpkt->data, 1, recvpkt->hdr.data_size, fp);
 
-            sndpkt = make_packet(0);
-            sndpkt->hdr.ackno = recvpkt->hdr.seqno + recvpkt->hdr.data_size;
-            next_seqno = sndpkt->hdr.ackno; // Update next_seqno to the hdr.ackno
+            // Update next_seqno to the next required pkt
+            next_seqno = recvpkt->hdr.seqno + recvpkt->hdr.data_size; 
 
             // Keep looping through receiver buffer till next required pkt with the correct ack is not found in the buffer
             int keep_reading = 0;
-            while ( 1 ) {
+            while (1) {
                 iter = 0;
                 keep_reading = 0; // Set to zero at each start till a matching pkt found in receiver buffer
                 while ( iter < buffer_size ) {
                     // If next required packet is already present in the receiver buffer
-                    if ( receiver_buffer[iter]->hdr.seqno == sndpkt->hdr.ackno )
-                    {
+                    if ( receiver_buffer[iter]->hdr.seqno == next_seqno ) {
                         // Write form the packet in the recevier buffer to the file
                         fseek(fp, receiver_buffer[iter]->hdr.seqno, SEEK_SET);
                         fwrite(receiver_buffer[iter]->data, 1, receiver_buffer[iter]->hdr.data_size, fp);
 
-                        sndpkt = make_packet(0);
-                        sndpkt->hdr.ackno = recvpkt->hdr.seqno + recvpkt->hdr.data_size;
-                        next_seqno = sndpkt->hdr.ackno; // Update next_seqno to the hdr.ackno
+                        // Update next_seqno to next required pkt
+                        next_seqno = recvpkt->hdr.seqno + recvpkt->hdr.data_size;; 
 
-                        // Set the writtend packet from buffer to be an empty packet
+                        // Set the written packet from buffer to be an empty packet
                         receiver_buffer[iter]->hdr.seqno = -1;
                         receiver_buffer[iter]->hdr.data_size = 0;
-                        keep_reading = 1;
+                        keep_reading = 1; // Set this to 1 if a matching pkt has been found
                     }
                     iter++;
                 }
                 // No matching packets were found in the buffer
-                if ( keep_reading == 0)
-                {
+                if ( keep_reading == 0) {
                     break;
                 }
             }
         }
-        printf("SEND PACKAGE LOOP next seqno = %i and recvpkt->hdr.seqno = %i and sndpck hdr ACK NO %i \n\n", next_seqno, recvpkt->hdr.seqno,sndpkt->hdr.ackno );
+        //printf("SEND PACKAGE LOOP next seqno = %i and recvpkt->hdr.seqno = %i and sndpck hdr ACK NO %i \n\n", next_seqno, recvpkt->hdr.seqno,sndpkt->hdr.ackno );
+        // For out of order packets, send orginial required packet ACK             
+        sndpkt = make_packet(0);
+        sndpkt->hdr.ackno = next_seqno;
         sndpkt->hdr.ctr_flags = ACK;
         if (sendto(sockfd, sndpkt, TCP_HDR_SIZE, 0, 
                 (struct sockaddr *) &clientaddr, clientlen) < 0) {
             error("ERROR in sendto");
         }
-        
-    }
-    // Loop to print out final contents of the buffer array
-    int ip =0;
-    while ( ip < buffer_size) {
-        printf("The Final hdr.seqno values in the array are %i\n", receiver_buffer[ip]->hdr.seqno );
-        ip++;
-
+        printf("%i receiver ack no \n", sndpkt->hdr.ackno );
     }
     return 0;
 }
