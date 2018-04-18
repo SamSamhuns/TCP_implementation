f93690c0b4b930fc3b7f4e8253da6bab74830912
sms1198 <sms1198@nyu.edu>
2018-04-18 07:03:34 -0700

    Added global pointer for next_seqno
    Added some global pointers for the resend function

--- rdt_sender.c
+++ rdt_sender.c
@@ -22,13 +22,15 @@
 #include "common.h"
 
 #define STDIN_FD 0
-#define RETRY 120 //milli second 
+#define RETRY 10 //milli second 
 #define max_window_size 100000// Setting the max_window_size to 1000 as a window_size CONST
 int window_size = 1;
 int dup_ack_seqno = 0; // To check if duplciate ackno has been sent from the receiver
 
 int next_seqno=0;
 int send_base=0;
+int *next_seqno_pointer;
+
 
 int sockfd, serverlen;
 struct sockaddr_in serveraddr;
@@ -40,25 +42,27 @@ sigset_t sigmask;
 // Buffer to hold the send window of window_size i.e. array of tcp_packets  
 tcp_packet *send_window[max_window_size];  
 
-void resend_packet() 
+void resend_packet( int *next_seqno ) 
 {
-    int index_to_send = 0;
-    index_to_send = send_base / DATA_SIZE;
+    // Calculates the index of the packet just after the last acked pkt
+    int index_to_send = send_base / DATA_SIZE;
 
     if(sendto(sockfd, send_window[index_to_send], TCP_HDR_SIZE + get_data_size(send_window[index_to_send]), 0, 
             ( const struct sockaddr *)&serveraddr, serverlen) < 0)
     {
         error("sendto");
-    };  
-
+    };
+    
+    window_size = 1;
+    //printf("send window seqno is %i and data size is %i  \n",send_window[index_to_send]->hdr.seqno, send_window[index_to_send]->hdr.data_size);
+    *next_seqno = send_window[index_to_send]->hdr.seqno + send_window[index_to_send]->hdr.data_size;
 }
 
 void signal_handler( int sig )
 {
     if(sig == SIGALRM){
-
-         VLOG(INFO, "Timout happend");
-         resend_packet();
+         VLOG(INFO, "00000000000000000000000000000000000000000000000000000000000000000000000000Timout happend");
+         resend_packet( next_seqno_pointer );
     }
 }
 
@@ -100,15 +104,13 @@ int main (int argc, char **argv)
     char buffer[DATA_SIZE]; // Send packet buffer
     char recv_buffer[DATA_SIZE]; // Receive packet buffer
     int dup_num = 0; // Variable for recording no of duplicate acks
-    int file_pointer = 0; // Keeps track of pointer that reads from the file
-    //int end_of_array = 0; // To denote the end of the sender buffer array i.e. it is a seqno
     int next_index = 0; //the index of the next packet to be sent 
     int current_index = 0; //the index of first packet in the window buffer
     int current_window_size = 0;
     int to_be_sent = 0; 
     FILE *fp;
 
-
+    next_seqno_pointer = &next_seqno;
     /* check command line arguments */
     if (argc != 4) {
         fprintf(stderr,"usage: %s <hostname> <port> <FILE>\n", argv[0]);
@@ -143,25 +145,11 @@ int main (int argc, char **argv)
 
     assert(MSS_SIZE - TCP_HDR_SIZE > 0);
 
-    //Stop and wait protocol
-
-    init_timer(RETRY, signal_handler);
-    next_seqno = 0;
-    send_base = 0;
-    int send_base_initial = 0; // base var to keep track of the base send for the first window_size packets
- 
-
-
     int position_buffer = 0; //position in the buffer where we are inserting the read packet
     int base = 0;
-    int seq_no = 0;
-
     while(1){
-
         len = fread(buffer, 1, DATA_SIZE,fp);
-
         if(len <= 0){
-
             sndpkt = make_packet(MSS_SIZE);
             sndpkt->hdr.data_size = 0;
             sndpkt->hdr.seqno = -1;
@@ -169,51 +157,30 @@ int main (int argc, char **argv)
             break;
         }
 
-
-        base = seq_no;
-        seq_no = base + len;
         sndpkt = make_packet(len);
         memcpy(sndpkt->data, buffer,len);
         sndpkt->hdr.seqno = base;
+        base += len;
 
         send_window[position_buffer] = (tcp_packet *)sndpkt;
         position_buffer ++;
-
     }
 
-
-
-    int k = 0;
-
-    while(1){
-
-        if(send_window[k]->hdr.seqno == -1){
-
-            printf("Exiting the loop 00 \n");
-            break;
-        }
-
-        printf("Seqno of packet is %d\n", send_window[k]->hdr.seqno);
-        k++;
-
-    }
-
+    //Stop and wait protocol
+    init_timer(RETRY, signal_handler );
+    next_seqno = 0;
+    send_base = 0;
+    int send_base_initial = 0; // base var to keep track of the base send for the first window_size packets
+ 
 
     // Send the first window_size packets
     position_buffer = 0;
     int i = 0;
     int length;
-    while ( i < window_size)
-    {
-
-
-
+    while ( i < window_size) {
         length = send_window[position_buffer]->hdr.data_size;
         send_base_initial = next_seqno;
-        next_seqno = send_base_initial +length;
-        //end_of_array = next_seqno;
-
-
+        next_seqno = send_base_initial + length;
 
         VLOG(DEBUG, "Sending packet %d to %s", 
         send_base_initial, inet_ntoa(serveraddr.sin_addr));
@@ -230,19 +197,12 @@ int main (int argc, char **argv)
 
         position_buffer++;
         i++;
-
     }
-
-    printf("Packet sent");
-
-
     
     // Main code for traversing the file and sending all the packets
     int eof_reach = 0; // End of file flag to check if the end of file has been reached in the sender's fread pointer
     //Wait for ACK
     do {   
-
-
         start_timer();
         //ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags,
         //struct sockaddr *src_addr, socklen_t *addrlen);
@@ -252,10 +212,9 @@ int main (int argc, char **argv)
             error("recvfrom");
         }
         recvpkt = (tcp_packet *) recv_buffer;
-        printf("%d \n", get_data_size(recvpkt));
+        //printf("%d \n", get_data_size(recvpkt));
         assert(get_data_size(recvpkt) <= DATA_SIZE);
 
-        
 
         //Test for triple duplicate acks
         if ( dup_ack_seqno == recvpkt->hdr.ackno )
@@ -269,35 +228,25 @@ int main (int argc, char **argv)
             dup_num = 0;
             printf("3 duplicate acks forced timeout\n");
             // If three duplicate 
-
-            resend_packet();
-
+            resend_packet( &next_seqno );
+            /*
             window_size = 1;
             current_index = send_base / DATA_SIZE;
             next_seqno = send_window[current_index]->hdr.seqno + send_window[current_index]->hdr.data_size;
-
-
-            
+            */   
             continue;         
         }
+        //printf("recvpkt ackno = %i, send_base =  %i\n", recvpkt->hdr.ackno, send_base  );
 
-        
-        printf("recvpkt ackno = %i, send_base =  %i\n", recvpkt->hdr.ackno, send_base  );
-
-        
         if ( recvpkt->hdr.ackno >= send_base ) {
             //printf("Inside this if statement\n");
-
-
             while ( send_base < recvpkt->hdr.ackno ) {
 
-    
- 
                 next_index = (int)ceil( (double)next_seqno / (double)DATA_SIZE ); //index of next packet to be sent
 
-                printf("NEXT PACKET TO BE SENT %d\n", next_index);
+                //printf("NEXT PACKET TO BE SENT %d\n", next_index);
 
-                printf("Index in our buffer %d\n",(int)ceil( (double)next_seqno / (double)DATA_SIZE ));
+                //printf("Index in our buffer %d\n",(int)ceil( (double)next_seqno / (double)DATA_SIZE ));
                 if ( send_window[next_index] ->hdr.seqno == -1){
 
                     if(send_window[next_index] ->hdr.data_size == 0){
@@ -312,20 +261,20 @@ int main (int argc, char **argv)
                 {
                     VLOG(INFO, "End of File has been reached");
                     sndpkt = make_packet(0);
+                    // Final zero_packet is sent multiple times to ensure that receiver gets this packet
+                    int resend_final_pkt = 0;
+                    while ( resend_final_pkt < 5) {
                     sendto(sockfd, sndpkt, TCP_HDR_SIZE,  0,
                         (const struct sockaddr *)&serveraddr, serverlen);
+                        resend_final_pkt++;
+                    }
                     return 0;
                 }
 
                 //printf("Inside if statement rcvpck ackno = %i, send_base =  %i\n",recvpkt->hdr.ackno, send_base );
-
-
                 current_index = send_base / DATA_SIZE; 
-
                 send_base = send_base + send_window[current_index]->hdr.data_size;
                 //printf("Send BASE IS %d\n", send_base);
-
-
 
                 VLOG(DEBUG, "Sending packet %d to %s\n", 
                 send_base , inet_ntoa(serveraddr.sin_addr));
@@ -345,43 +294,29 @@ int main (int argc, char **argv)
                     {
                         error("sendto");
                     }
-                    printf("Sending packet 0000000000000\n");
                     next_seqno += send_window[next_index]->hdr.data_size;
-                    
                     window_size++;
                 }
-
-
             }
 
-            printf("111111111111111111111111\n");
-
-            printf("Send base is%d\n", send_base);
-            printf("Nextseqno is %d\n", next_seqno);
+            //printf("Send base is %d\n", send_base);
+            //printf("Next seqno is %d\n", next_seqno);
             current_window_size = (int)ceil ((double)(next_seqno - send_base)/ (double)DATA_SIZE);
             to_be_sent = window_size - current_window_size;
 
             printf("Before loop Current window size is %d\n", current_window_size);
 
             int check = 0;//check for end of file
-
             while(to_be_sent > 0){
 
                 next_index = (int)ceil( (double)next_seqno / (double)DATA_SIZE );
 
-                printf("INSIDE THE LOOP\n");
-
-
                 if ( send_window[next_index] ->hdr.seqno == -1){
-
                     if(send_window[next_index] ->hdr.data_size == 0){
-
                         check= 1;
                         break;
                      }   
-
                 }
-
 
                 if(check != 1){
                     if(sendto(sockfd, send_window[next_index], TCP_HDR_SIZE + get_data_size(send_window[next_index]), 0, 
@@ -389,23 +324,14 @@ int main (int argc, char **argv)
                         {
                             error("sendto");
                         }
-                        printf("Sending packet 0000000000000\n");
-                        next_seqno += send_window[next_index]->hdr.data_size;
+                    next_seqno += send_window[next_index]->hdr.data_size;
                 }
-
                 to_be_sent--;
-
-
             }
 
             current_window_size = (int)ceil ((double)(next_seqno - send_base)/ (double)DATA_SIZE);
-
             printf("After loop Current window size is %d\n", current_window_size);
 
-
-
-
-            //printf("11111111111111111\n");
         }
         stop_timer();
     } while(1);
@@ -413,5 +339,4 @@ int main (int argc, char **argv)
     // Control should never reach this
     return 0;
 
-    
 }
